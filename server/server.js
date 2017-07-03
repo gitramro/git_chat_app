6973445742bdf1ea32a03f5cd29255266e31032c
Ramón Oscar <ramon.osc93@gmail.com>
2017-07-03 11:18:05 -0700

    add betalike broadcast chat

--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{ //event listener
+	console.log('New user connected');
 
-app.listen(port,()=>{
+
+	socket.on('createMessage',(message)=>{
+		console.log('Create message',message);
+		io.emit('newMessage',{
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
+	});
+
+	socket.on('disconnect',()=>{
+		console.log('Client disconnected');
+	});
+}); 
+
+server.listen(port,()=>{
 	console.log(`Server is up on ${port}`);
 });
